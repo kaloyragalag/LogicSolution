c2ccd20e86892e99f61783fdae2f05d32bed1066
Karl Galagar <kfgalagar@yahoo.com>
2022-09-16 05:40:55 +0800

    add test solution and move to model folder

---
+++ LogicSolution.Test/CodeWarsServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using LogicSolution.Services;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicSolution.Test
+{
+    [TestFixture]
+    public class CodeWarsServiceTest
+    {
+        private ICodeWarsService codeWarsService;
+        [SetUp]
+        public void Setup()
+        {
+            codeWarsService = new CodeWarsService();
+        }
+    }
+}
